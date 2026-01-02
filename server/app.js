55e0a0f0851de3b509e8a7fa043bc44d1f0ade84
GoaimaElisabetaAndreea <elisabetagoaima@gmail.com>
2026-01-02 15:55:01 +0200

    Inițializare proiect Express

---
+++ server/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+const port =  process.env.PORT || 3000;
+
+// middleware
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.json({message: "Running"})
+});
+
+app.listen(port, () => {
+    console.log('Server running');
+});
+
